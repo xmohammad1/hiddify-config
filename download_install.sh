b16e1b68e6ea1966ffbc377ca6657522a0dd8cc1
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2024-01-07 21:18:43 +0330

    Update download_install.sh

--- download_install.sh
+++ download_install.sh
@@ -4,10 +4,6 @@ if [ "$(id -u)" -ne 0 ]; then
         exit 1
 fi
 export DEBIAN_FRONTEND=noninteractive
-if [ "$(id -u)" -ne 0 ]; then
-        echo 'This script must be run by root' >&2
-        exit 1
-fi
 
 echo "we are going to download needed files:)"
 GITHUB_REPOSITORY=hiddify-config
@@ -16,18 +12,20 @@ GITHUB_BRANCH_OR_TAG=main
 
 # if [ ! -d "/opt/$GITHUB_REPOSITORY" ];then
 apt update
-apt upgrade -y
+#apt upgrade -y
+apt -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+
 apt install -y curl unzip
 # pip3 install lastversion "requests<=2.29.0"
 # pip install lastversion "requests<=2.29.0"
 mkdir -p /opt/$GITHUB_REPOSITORY
 cd /opt/$GITHUB_REPOSITORY
-curl -L -o hiddify-config.zip https://github.com/hiddify/hiddify-config/releases/latest/download/hiddify-config.zip
-unzip -o hiddify-config.zip
-rm hiddify-config.zip
+curl -L -o $GITHUB_REPOSITORY.zip https://github.com/hiddify/$GITHUB_REPOSITORY/releases/latest/download/$GITHUB_REPOSITORY.zip
+unzip -o $GITHUB_REPOSITORY.zip
+rm $GITHUB_REPOSITORY.zip
 sed -i 's/ "requests<=2.29.0"//g' /opt/hiddify-config/hiddify-panel/install.sh
-#sed -i 's/--version 1.8.4/--version 1.8.6/g' /opt/hiddify-config/xray/install.sh
-#sed -i 's/--version 1.8.4/--version 1.8.6/g' /opt/hiddify-config/install.sh
+# sed -i 's/--version 1.8.4/--version 1.8.6/g' /opt/hiddify-config/xray/install.sh
+# sed -i 's/--version 1.8.4/--version 1.8.6/g' /opt/hiddify-config/install.sh
 sed -i ':a;N;$!ba;s/"forbidden_sites",\n\s* "protocol": "blackhole"/"forbidden_sites", "protocol": "blackhole"/' /opt/hiddify-config/xray/configs/06_outbounds.json.template
 sed -i 's/"8.8.8.8",/"8.8.4.4",/g' /opt/hiddify-config/xray/configs/02_dns.json.template
 bash install.sh
